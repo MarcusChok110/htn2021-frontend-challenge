bb91bfc1c14bb0816a58f639d5ccdf530c18d1c9
Marcus Chok <marcus.chok110@gmail.com>
2021-03-11 23:36:21 -0500

    Add EventCards on Grid to display event data

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,85 @@
+import {
+  Grid,
+  InputAdornment,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+import React, { useContext, useEffect } from 'react';
+import EventCard from '../../components/EventCard';
+import useTextField from '../../components/Form/useTextField';
+import EventContext from '../../contexts/EventContext';
+import UserContext from '../../contexts/UserContext';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    margin: theme.spacing(2),
+  },
+}));
+
+const Home: React.FC = () => {
+  const events = useContext(EventContext);
+  const [isLoggedIn] = useContext(UserContext);
+  const classes = useStyles();
+  const [search, searchProps, SearchInput] = useTextField('Search Events');
+
+  // sort by start_time and filter by search input
+  const filteredEvents = events
+    .sort((a, b) => a.start_time - b.start_time)
+    .filter((event) => {
+      return event.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+  useEffect(() => {
+    console.log(events);
+  }, [events]);
+
+  return (
+    <div>
+      <Grid container justify="space-between" alignItems="center">
+        <Grid item>
+          <Typography className={classes.title} variant="h2">
+            Events
+          </Typography>
+        </Grid>
+        <Grid item>
+          <SearchInput
+            {...searchProps}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container alignItems="stretch" direction="column">
+        {filteredEvents &&
+          filteredEvents.map((event) => {
+            if (
+              (event.permission === 'private' && isLoggedIn) ||
+              event.permission === 'public'
+            ) {
+              const { id, name, description, start_time } = event;
+              return (
+                <Grid item>
+                  <EventCard
+                    key={event.id}
+                    id={id}
+                    name={name}
+                    description={description}
+                    start_time={start_time}
+                  />
+                </Grid>
+              );
+            }
+            return null;
+          })}
+      </Grid>
+    </div>
+  );
+};
+
+export default Home;
