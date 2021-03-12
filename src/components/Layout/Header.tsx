15cc6053274e4afee5aa9c664523043483cc36da
Marcus Chok <marcus.chok110@gmail.com>
2021-03-12 17:27:41 -0500

    Add logout handling to logout button

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -7,8 +7,10 @@ import {
   Typography,
 } from '@material-ui/core';
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import UserContext from '../../contexts/UserContext';
+import { userActions } from '../../utils/reducers/userReducer';
+import useSnackbar from '../Form/useSnackBar';
 
 // To get rid of Link's default styles on the title
 const useStyles = makeStyles({
@@ -19,31 +21,45 @@ const useStyles = makeStyles({
 });
 
 const Header: React.FC = () => {
-  const [isLoggedIn] = useContext(UserContext);
   const classes = useStyles();
+  const history = useHistory();
+  const [isLoggedIn, dispatch] = useContext(UserContext);
+  const [openSnackbar, snackbarProps, LogoutSnackbar] = useSnackbar(
+    'Logged out successfully'
+  );
+  const handleLogout = () => {
+    dispatch(userActions.LOGOUT);
+    openSnackbar();
+    history.push('/');
+  };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Grid container justify="space-between">
-          <Typography
-            className={classes.title}
-            variant="h6"
-            component={Link}
-            to="/"
-          >
-            Hackathon Global Inc
-          </Typography>
-          {isLoggedIn ? (
-            <Button color="inherit">Logout</Button>
-          ) : (
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-          )}
-        </Grid>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container justify="space-between">
+            <Typography
+              className={classes.title}
+              variant="h6"
+              component={Link}
+              to="/"
+            >
+              Hackathon Global Inc
+            </Typography>
+            {isLoggedIn ? (
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            ) : (
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+            )}
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <LogoutSnackbar {...snackbarProps} />
+    </>
   );
 };
 
