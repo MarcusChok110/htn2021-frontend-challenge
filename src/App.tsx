812fd9ed68f195c6d932c15b14f08da2e0717111
Marcus Chok <marcus.chok110@gmail.com>
2021-03-11 23:37:22 -0500

    Add Context Providers, routing, and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer, useState } from 'react';
+import fetchEvents from './api/fetchEvents';
+import EventContext from './contexts/EventContext';
+import Routes from './routes';
+import { TEndpointResponse } from './types/events';
+import 'fontsource-roboto';
+import userReducer from './utils/reducers/userReducer';
+import UserContext from './contexts/UserContext';
+import Layout from './components/Layout';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
+  const [events, setEvents] = useState<TEndpointResponse>([]);
+  const [isLoggedIn, dispatch] = useReducer(userReducer, false);
+
+  useEffect(() => {
+    fetchEvents().then((response) => setEvents(response));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EventContext.Provider value={events}>
+      <UserContext.Provider value={[isLoggedIn, dispatch]}>
+        <BrowserRouter>
+          <Layout>
+            <Routes />
+          </Layout>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </EventContext.Provider>
   );
-}
+};
 
 export default App;
