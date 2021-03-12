e9f4cdfe42c684ab0d9299c588846edd56671f4c
Marcus Chok <marcus.chok110@gmail.com>
2021-03-12 16:54:00 -0500

    Move to utils folder

--- src/utils/api/fetchEvents.ts
+++ src/utils/api/fetchEvents.ts
@@ -1,4 +1,4 @@
-import { TEndpointResponse } from '../types/events';
+import { TEndpointResponse } from '../../types/events';
 
 export const eventsURL =
   'https://api.hackthenorth.com/v3/graphql?query={ events { id name event_type permission start_time end_time description speakers { name profile_pic } public_url private_url related_events } }';
