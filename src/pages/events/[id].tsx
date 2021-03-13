90d2eeeb659fd515b6db315c424f09f0ed070e5c
Marcus Chok <marcus.chok110@gmail.com>
2021-03-12 23:10:19 -0500

    Add event types and urls, and simplify rendering

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -16,13 +16,10 @@ import EventContext from '../../contexts/EventContext';
 import UserContext from '../../contexts/UserContext';
 import findEvent from '../../utils/helpers/findEvent';
 import toDateAndLocaleTimeString from '../../utils/helpers/toDateAndLocaleTimeString';
+import eventTypeToTitleCase from '../../utils/helpers/eventTypeToTitleCase';
 
 const useStyles = makeStyles((theme) => ({
   title: {
-    margin: theme.spacing(2, 0),
-  },
-  paper: {
-    padding: theme.spacing(2),
     margin: theme.spacing(2, 0),
   },
 }));
@@ -44,9 +41,13 @@ const EventPage: React.FC = () => {
     end_time,
     related_events,
     speakers,
+    event_type,
+    public_url,
+    private_url,
   } = event;
   const startDate = new Date(start_time);
   const endDate = new Date(end_time);
+  // filter out undefined events and private events if not logged in
   const viewableRelatedEvents = related_events
     .map((event) => findEvent(events, event))
     .filter((event) => event && (event.permission === 'public' || isLoggedIn));
@@ -62,52 +63,77 @@ const EventPage: React.FC = () => {
       >{`${toDateAndLocaleTimeString(startDate)} - ${toDateAndLocaleTimeString(
         endDate
       )} (${Intl.DateTimeFormat().resolvedOptions().timeZone})`}</Typography>
-      <Paper className={classes.paper} elevation={3}>
+      <Paper elevation={3}>
         <Typography
           gutterBottom
           variant="body1"
           color="textPrimary"
           component="p"
         >
-          {description ?? null}
+          <List>
+            {description && (
+              <>
+                <ListItem>
+                  <ListItemText>{description}</ListItemText>
+                </ListItem>
+                <Divider />
+              </>
+            )}
+            {public_url && (
+              <ListItem>
+                <ListItemText>
+                  Learn More: <Link href={public_url}>{public_url}</Link>
+                </ListItemText>
+              </ListItem>
+            )}
+            {isLoggedIn && (
+              <ListItem>
+                <ListItemText>
+                  Super Secret Hacker Link:{' '}
+                  <Link href={private_url}>{private_url}</Link>
+                </ListItemText>
+              </ListItem>
+            )}
+          </List>
         </Typography>
       </Paper>
       <List>
         <ListItem>
           <ListItemText
-            primary={<Typography variant="h6">Speakers: </Typography>}
-            secondary={
-              <>
-                {speakers.map((speaker, index) => (
-                  <span key={index}>
-                    {speaker.name}
-                    {index < speakers.length - 1 ? ', ' : ''}
-                  </span>
-                ))}
-              </>
-            }
+            primary={<Typography variant="h6">Event Type</Typography>}
+            secondary={eventTypeToTitleCase(event_type)}
           />
         </ListItem>
-        <Divider />
-        <ListItem>
-          <ListItemText
-            primary={<Typography variant="h6">Related Events: </Typography>}
-            secondary={
-              <>
-                {viewableRelatedEvents.map((value, index) => (
-                  <Link
-                    key={index}
-                    component={RouterLink}
-                    to={`/events/${value?.id}`}
-                  >
-                    {value?.name}
-                    {index < viewableRelatedEvents.length - 1 ? ', ' : ''}
-                  </Link>
-                ))}
-              </>
-            }
-          />
-        </ListItem>
+        {speakers.length > 0 && (
+          <ListItem>
+            <ListItemText
+              primary={<Typography variant="h6">Speakers</Typography>}
+              secondary={speakers.map((speaker, index) => (
+                <span key={index}>
+                  {speaker.name}
+                  {index < speakers.length - 1 ? ', ' : ''}
+                </span>
+              ))}
+            />
+          </ListItem>
+        )}
+        {viewableRelatedEvents.length > 0 && (
+          <ListItem>
+            <ListItemText
+              primary={<Typography variant="h6">Related Events</Typography>}
+              secondary={viewableRelatedEvents.map((value, index) => (
+                <Link
+                  key={index}
+                  component={RouterLink}
+                  to={`/events/${value?.id}`}
+                >
+                  {value?.name}
+                  {index < viewableRelatedEvents.length - 1 ? ', ' : ''}
+                </Link>
+              ))}
+            />
+          </ListItem>
+        )}
       </List>
     </>
   );
